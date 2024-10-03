11a4bf1ebf974b6c272beeaf0befba867e7ed26b
playerhacks <163364156+playerhacks@users.noreply.github.com>
2024-10-03 01:32:42 -0700

    Update dictionary.js
    added more words
    
    Signed-off-by: playerhacks <163364156+playerhacks@users.noreply.github.com>

--- dictionary.js
+++ dictionary.js
@@ -15,7 +15,7 @@ externalDictionary = [
             }
         ]
     },
-     {
+    {
         "word": "Fare",
         "uses": [
             {
@@ -54,6 +54,155 @@ externalDictionary = [
                 "example": "Na.",
                 "gloss": "help-less-ness",
                 "exampleTranslation": "No."
+            }
+        ]
+    },
+    {
+        "word": "jan",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "an individual or human",
+                "example": "Salu, jan.",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "Hello, human."
+            },
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "jan ale",
+                "example": "multiple individuals or humans.",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "bon'ja",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "a living organism of the kind exemplified by trees, shrubs, herbs, grasses, ferns, and mosses.",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "hon-bon'ja",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "a woody perennial plant, typically having a single stem or trunk growing to a considerable height and bearing lateral branches at some distance from the ground.",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": " kecil jan",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "a very young child, especially one newly or recently born.",
+                "example": "Salu, kecil jan.",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "Hello, baby."
+            }
+        ]
+    },
+    {
+        "word": "hon jan",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "an adult male human being.",
+                "example": "Salu, hon jan!",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "Hello, man!"
+            }
+        ]
+    },
+    {
+        "word": "gü'ka",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "to cut down or chop, usually with an axe.",
+                "example": "hon jën-ɮa gü'ka hon-bon'ja.",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "The man cut down the tree."
+            }
+        ]
+    },
+    {
+        "word": "kecil",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "mouse",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "hon",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "elephant",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "dom'ka",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "NOUN",
+                "explanation": "animal",
+                "example": "",
+                "gloss": "help-less-ness",
+                "exampleTranslation": ""
+            }
+        ]
+    },
+    {
+        "word": "bem",
+        "uses": [
+            {
+                "hasBadge": false,
+                "badgeText": "",
+                "type": "VERB",
+                "explanation": "to kill",
+                "example": "Kecil bem hon.",
+                "gloss": "help-less-ness",
+                "exampleTranslation": "Mouse kills the elephant."
             }
         ]
     }
