d04cdb635bf4f52e7c8974aa658102e894657858
playerhacks <163364156+playerhacks@users.noreply.github.com>
2024-10-02 22:24:55 -0700

    Update dictionary.js
    Starter words

--- dictionary.js
+++ dictionary.js
@@ -2,7 +2,7 @@ dictionaryDescription = "This dictionary contains the words I know from Sanā.",
 dictionaryHaveGlosses = false,
 externalDictionary = [
     {
-        "word": "Ahora",
+        "word": "Salu",
         "uses": [
             {
                 "hasBadge": false,
@@ -16,21 +16,7 @@ externalDictionary = [
         ]
     },
     {
-        "word": "Vu",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "NOUN",
-                "explanation": "Used to describe the person who you are speaking to.",
-                "example": "Ahora-vu!",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "Hello, you!"
-            }
-        ]
-    },
-    {
-        "word": "Sa",
+        "word": "Ta",
         "uses": [
             {
                 "hasBadge": false,
@@ -40,20 +26,11 @@ externalDictionary = [
                 "example": "Sa.",
                 "gloss": "help-less-ness",
                 "exampleTranslation": "Yes."
-            },
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "ADVERB",
-                "explanation": "Sabon",
-                "example": "Another way to say yes. (might be stressed or more extreme)",
-                "gloss": "help-less-ness",
-                "exampleTranslation": ""
             }
         ]
     },
     {
-        "word": "Na",
+        "word": "Nai",
         "uses": [
             {
                 "hasBadge": false,
@@ -63,164 +40,6 @@ externalDictionary = [
                 "example": "Na.",
                 "gloss": "help-less-ness",
                 "exampleTranslation": "No."
-            }
-        ]
-    },
-    {
-        "word": "Ji",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "PRONOUN",
-                "explanation": "Used to talk about yourself.",
-                "example": "Ji...",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "I..."
-            }
-        ]
-    },
-    {
-        "word": "Ka",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "?",
-                "explanation": "Used to ask about something.",
-                "example": "Ka ahora?",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "What does ahora (hello) mean?"
-            }
-        ]
-    },
-    {
-        "word": "Es",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "VERB (I think)",
-                "explanation": "idk",
-                "example": "Sabon es sa.",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "Sabon is sa."
-            }
-        ]
-    },
-    {
-        "word": "farhem",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "VERB",
-                "explanation": "Expresses whether you are aware of what something means. ",
-                "example": "Ji farhem",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "I know"
-            },
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "VERB",
-                "explanation": "Nar'farhem",
-                "example": "The negative version of the word.",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "I don't know"
-            }
-        ]
-    },
-    {
-        "word": "Dankje",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "NOUN",
-                "explanation": "Canine",
-                "example": "Ahora, dankje!",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "Hello, dog!"
-            }
-        ]
-    },
-    {
-        "word": "Garro",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "NOUN",
-                "explanation": "Feline",
-                "example": "Ahora, garro!",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "Hello, cat!"
-            },
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "NOUN",
-                "explanation": "Gatito",
-                "example": "Another way to say garro.",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "Example of this usage (Translation)"
-            }
-        ]
-    },
-    {
-        "word": "lutem",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "INTERJECTION",
-                "explanation": "Expresses gratitude.",
-                "example": "lutem!!",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "Thanks!!"
-            }
-        ]
-    },
-    {
-        "word": "Sitweb",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "NOUN",
-                "explanation": "a set of related web pages located under a single domain name, typically produced by a single person or organization.",
-                "example": "ka sitweb es i?",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "What website is it?"
-            }
-        ]
-    },
-    {
-        "word": "I",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "NOUN",
-                "explanation": "it",
-                "example": "ka es i",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "What is it?"
-            }
-        ]
-    },
-    {
-        "word": "Sai",
-        "uses": [
-            {
-                "hasBadge": false,
-                "badgeText": "",
-                "type": "VERB",
-                "explanation": "The ability to talk/express feelings",
-                "example": "Ji sai.",
-                "gloss": "help-less-ness",
-                "exampleTranslation": "I speak."
             }
         ]
     }
